9e7184f009e6935e49a42341cec7bf3733860fa7
Valette Eliott <156651552+eliottvalette@users.noreply.github.com>
2025-01-06 23:36:46 +0100

    Update word lists and enhance input handling: Added new entries to failed-words.txt and valid_targets.txt for improved recognition. Refactored input_with_timeout function to use threading for better timeout management and removed the old implementation from utils.py.

--- hacker.io.py
+++ hacker.io.py
@@ -16,14 +16,34 @@ import random
 import json
 import math
 
-from utils import input_with_timeout
 from time import sleep
+import threading
+
 
 load_dotenv()
 username = 'Cutie160053'
 email = 'sitif93205@pofmagic.com'
 
 password = os.getenv("PASSWORD")
+
+def input_with_timeout(prompt, timeout):
+    user_input = [None]  # Use a mutable object to store the input
+
+    def get_input():
+        user_input[0] = input(prompt)
+
+    # Start a thread to get input
+    thread = threading.Thread(target=get_input)
+    thread.start()
+    thread.join(timeout)  # Wait for the thread to finish or timeout
+
+    if thread.is_alive():
+        # If the thread is still running, input timed out
+        print("\nInput timed out!")
+        return 'n'
+
+    return user_input[0]
+
 class HackerIOBot:
     def __init__(self):
         self.driver = None
